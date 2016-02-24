cb6841c0b7e457d997193a9ce4a33109186bc5be
HaGiang994 <gianglehongha2805@gmail.com>
2016-02-24 11:40:08 +0700

    Add corrected file
    This is file MaxValueDivideBy2

---
+++ MaxValueDivideBy2/MaxValueDivideBy2/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  MaxValueDivideBy2
+//
+//  Created by MacPro1 on 2/24/16.
+//  Copyright © 2016 MacPro1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleClass : NSObject
+- (NSInteger) maxValueDivide2: (NSArray *)array;
+@end
+
+@implementation SimpleClass
+- (NSInteger) maxValueDivide2: (NSArray *)array {
+    NSInteger maxValue = [[array objectAtIndex:0] intValue];
+    NSInteger numberOfElements = [array count];
+    for (int i = 1; i < numberOfElements; i ++) {
+        if (maxValue < [[array objectAtIndex:i] intValue] && [[array objectAtIndex:i] intValue] % 2 == 0) {
+            maxValue = [[array objectAtIndex:i] intValue];
+        }
+    }
+    return maxValue;
+}
+@end
+
+int main(int argc, const char * argv[]) {
+    NSArray *array = @[@1,@2,@3,@4,@5,@6,@8,@9,@100,@120,@99,@98];
+    SimpleClass *simpleClass = [SimpleClass alloc];
+    NSLog(@"Max number is divided by 2 in array = %ld",[simpleClass maxValueDivide2:array]);
+    return 0;
+}
